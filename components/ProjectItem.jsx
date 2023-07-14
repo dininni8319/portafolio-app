5ecdc9cbdc25ef08fe0d32b2a7e52a95b23d19cc
Salvatore Dininni <s.dininni@yahoo.com>
2023-07-14 13:48:23 +0200

    style: style images optimazation

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,8 +4,10 @@ import Link from 'next/link'
 
 const ProjectItem = ({ src, title, link, description }) => {
   return (
-    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">  
-          <Image className='rounded-xl group-hover:opacity-20 h-80' src={src} alt='/' width={500} height={500}/>
+    <div className="relative flex items-center justify-center w-90 shadow-xl shadow-gray-400 rounded-xl p-2 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">  
+          <div className='card-project'>
+             <Image className='rounded-xl group-hover:opacity-20 image' src={src} alt='/'/>
+          </div>
           <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
             <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{description}</p>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
       <div className="max-w-[1240px] mx-auto px-2 py-16">
         <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
         <h2 className='py-4'>What {"I have"} Built</h2>
-        <div className="grid md:grid-cols-2 gap-8">
+        <div className="grid md:grid-cols-2 gap-14">
             <ProjectItem
               src={ecommerce}
               title='E-commerce'
