fbd02502d94e3085cffdffbf4a72354342b69796
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2024-08-29 17:28:07 -0700

    updated about section and experiences

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -26,25 +26,6 @@ const Experience = () => {
                       <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
                   </div>
                   <div className="mt-3 sm:pr-8 shadow-xl shadow-gray-400 rounded-xl p-2">
-                    <h4 className="text-lg font-semibold text-gray-900">Web Developer</h4>
-                      <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February, 2024</time>
-                      <p className="text-base font-normal text-gray-500 dark:text-gray-400">Freelancer</p>
-                  </div>
-              </motion.li>
-               <motion.li 
-                 initial={{ opacity: 0 }}
-                 whileInView={{ opacity: 1 }}
-                 viewport={{ once: true }} 
-                 className="relative mb-6 sm:mb-0">
-                  <div className="flex items-center">
-                      <div className="z-10 flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full ring-0 ring-white dark:bg-blue-900 sm:ring-8 dark:ring-gray-900 shrink-0">
-                          <svg className="w-2.5 h-2.5 text-[#5651e5] dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                          </svg>
-                      </div>
-                      <div className="hidden sm:flex w-full bg-gray-200 h-0.5 dark:bg-gray-700"></div>
-                  </div>
-                  <div className="mt-3 sm:pr-8 shadow-xl shadow-gray-400 rounded-xl p-2">
                     <h4 className="text-lg font-semibold text-gray-900">Codebar.io</h4>
                       <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">August, 2023</time>
                       <p className="text-base font-normal text-gray-500 dark:text-gray-400">Coach</p>
